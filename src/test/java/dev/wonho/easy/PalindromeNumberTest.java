5c40adb4c4c3da7a0098e703cf6ceeeb34b1a7b2
Wonho Lee <wonho.dev@gmail.com>
2020-01-21 22:34:51 -0500

    Add 9.Palindrome Number

---
+++ src/test/java/dev/wonho/easy/PalindromeNumberTest.java
@@ -0,0 +1,65 @@
+package dev.wonho.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PalindromeNumberTest {
+
+    private PalindromeNumber palindromeNumber = new PalindromeNumber();
+
+    @Test
+    public void test_Negative() {
+        int input = -121;
+        assertThat(palindromeNumber.isPalindrome(input))
+                .as("Input: " + input)
+                .isFalse();
+    }
+
+    @Test
+    public void test_Zero() {
+        int input = 0;
+        assertThat(palindromeNumber.isPalindrome(input))
+                .as("Input: " + input)
+                .isTrue();
+    }
+
+    @Test
+    public void test_OneDigit() {
+        int input = 4;
+        assertThat(palindromeNumber.isPalindrome(input))
+                .as("Input: " + input)
+                .isTrue();
+    }
+
+    @Test
+    public void test_Examples_True() {
+        int input = 121;
+        assertThat(palindromeNumber.isPalindrome(input)).as("Input: " + input).isTrue();
+
+        input = 1221;
+        assertThat(palindromeNumber.isPalindrome(input)).as("Input: " + input).isTrue();
+
+        input = 2649462;
+        assertThat(palindromeNumber.isPalindrome(input)).as("Input: " + input).isTrue();
+
+        input = 26499462;
+        assertThat(palindromeNumber.isPalindrome(input)).as("Input: " + input).isTrue();
+    }
+
+    @Test
+    public void test_Examples_False() {
+        int input = 10;
+        assertThat(palindromeNumber.isPalindrome(input)).as("Input: " + input).isFalse();
+
+        input = 123;
+        assertThat(palindromeNumber.isPalindrome(input)).as("Input: " + input).isFalse();
+
+        input = 1011;
+        assertThat(palindromeNumber.isPalindrome(input)).as("Input: " + input).isFalse();
+
+        input = 26498462;
+        assertThat(palindromeNumber.isPalindrome(input)).as("Input: " + input).isFalse();
+    }
+
+}
