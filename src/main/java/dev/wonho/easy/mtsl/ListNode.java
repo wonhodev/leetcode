920551d9c65dcf1286f32cb6f0145b952cc77e32
Wonho Lee <wonho.dev@gmail.com>
2020-01-25 21:53:18 -0500

    Add 21. Merge Two Sorted Lists

---
+++ src/main/java/dev/wonho/easy/mtsl/ListNode.java
@@ -0,0 +1,12 @@
+package dev.wonho.easy.mtsl;
+
+public class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+
+}
---
+++ src/test/java/dev/wonho/easy/mtsl/SortedListTest.java
@@ -0,0 +1,69 @@
+package dev.wonho.easy.mtsl;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SortedListTest {
+
+    @Test
+    public void test_createList() {
+        int[] input = { 1, 2, 4 };
+        ListNode list = SortedList.createList(input);
+
+        assertThat(list.val).as("1, 2, 4: " + list.val).isEqualTo(1);
+
+        list = list.next;
+        assertThat(list.val).as("1, 2, 4: " + list.val).isEqualTo(2);
+
+        list = list.next;
+        assertThat(list.val).as("1, 2, 4: " + list.val).isEqualTo(4);
+    }
+
+    @Test
+    public void test_createList_Null() {
+        assertThat(SortedList.createList(null))
+                .as("null")
+                .isEqualTo(null);
+    }
+
+    @Test
+    public void test_createList_Empty() {
+        int[] input = { };
+
+        assertThat(SortedList.createList(input))
+                .as("{ }")
+                .isEqualTo(null);
+    }
+
+    @Test
+    public void test_toString() {
+        int[] input = { 1, 2, 4 };
+        ListNode list = SortedList.createList(input);
+
+        assertThat(SortedList.toString(list))
+                .as(Arrays.toString(input))
+                .isEqualTo(Arrays.toString(input));
+    }
+
+    @Test
+    public void test_toString_Null() {
+        assertThat(SortedList.toString(null))
+                .as("null")
+                .isEqualTo(null);
+    }
+
+    @Test
+    public void test_toString_Empty() {
+        int[] input = { };
+        ListNode list = SortedList.createList(input);
+
+        assertThat(SortedList.toString(list)).as("{ }").isEqualTo(null);
+
+        list = SortedList.createList();
+        assertThat(SortedList.toString(list)).isEqualTo(null);
+    }
+
+}
